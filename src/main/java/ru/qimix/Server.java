ede366604fa844dee44b12e613cfea84f3d00109
Vitaliy Murzin <murzinvit@yandex.ru>
2024-08-15 02:06:43 +0500

    add threadpool

--- src/main/java/ru/qimix/Server.java
+++ src/main/java/ru/qimix/Server.java
@@ -5,43 +5,35 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Server {
     final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
+    ExecutorService threadPool = Executors.newFixedThreadPool(64);
 
     public void startServer() {
         try (final var serverSocket = new ServerSocket(9999)) {
             while (true) {
-                try (
-                        final var socket = serverSocket.accept();
-                        final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-                        final var out = new BufferedOutputStream(socket.getOutputStream());
-                ) {
-                    final var requestLine = in.readLine();
-                    final var parts = requestLine.split(" ");
-
-                    if (parts.length != 3) {
-                        continue;
-                    }
-
-                    final var path = parts[1];
-                    if (!validPaths.contains(path)) {
-                        out.write((
-                                "HTTP/1.1 404 Not Found\r\n" +
-                                        "Content-Length: 0\r\n" +
-                                        "Connection: close\r\n" +
-                                        "\r\n"
-                        ).getBytes());
-                        out.flush();
-                        continue;
-                    } else {
-                        Request request = new Request(parts[0], parts[1], parts[2], serverSocket, in, out);
-                        connectionProcess(request);
-                    }
+                try {
+                    final var socket = serverSocket.accept();
+                    threadPool.submit(new Runnable() {
+                        @Override
+                        public void run() {
+                            try {
+                                connectionProcess(socket);
+                            } catch (IOException e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                    });
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
                 }
             }
         } catch (IOException e) {
@@ -49,37 +41,60 @@ public class Server {
         }
     }
 
-    public void connectionProcess(Request request) throws IOException {
-        final var filePath = Path.of(".", "public", request.path);
+
+    public void connectionProcess(Socket socket) throws IOException {
+        final var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+        final var out = new BufferedOutputStream(socket.getOutputStream());
+        final var requestLine = in.readLine();
+        final var parts = requestLine.split(" ");
+
+        if (parts.length != 3) {
+            return;
+        }
+
+        final var path = parts[1];
+        if (!validPaths.contains(path)) {
+            out.write((
+                    "HTTP/1.1 404 Not Found\r\n" +
+                            "Content-Length: 0\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            out.flush();
+            return;
+        }
+
+
+        final var filePath = Path.of(".", "public", path);
         final var mimeType = Files.probeContentType(filePath);
 
         // special case for classic
-        if (request.path.equals("/classic.html")) {
+        if (path.equals("/classic.html")) {
             final var template = Files.readString(filePath);
             final var content = template.replace(
                     "{time}",
                     LocalDateTime.now().toString()
             ).getBytes();
-            request.out.write((
+            out.write((
                     "HTTP/1.1 200 OK\r\n" +
                             "Content-Type: " + mimeType + "\r\n" +
                             "Content-Length: " + content.length + "\r\n" +
                             "Connection: close\r\n" +
                             "\r\n"
             ).getBytes());
-            request.out.write(content);
-            request.out.flush();
+            out.write(content);
+            out.flush();
         }
 
         final var length = Files.size(filePath);
-        request.out.write((
+        out.write((
                 "HTTP/1.1 200 OK\r\n" +
                         "Content-Type: " + mimeType + "\r\n" +
                         "Content-Length: " + length + "\r\n" +
                         "Connection: close\r\n" +
                         "\r\n"
         ).getBytes());
-        Files.copy(filePath, request.out);
-        request.out.flush();
+        Files.copy(filePath, out);
+        out.flush();
     }
 }
