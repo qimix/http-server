fa6740c9caf41795937edc49a3abf9aa25e4e999
Vitaliy Murzin <murzinvit@yandex.ru>
2024-08-17 15:30:13 +0500

    add handlerMap

--- src/main/java/ru/qimix/Main.java
+++ src/main/java/ru/qimix/Main.java
@@ -5,6 +5,7 @@ import java.net.ServerSocket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
 
 public class Main {
--- src/main/java/ru/qimix/Server.java
+++ src/main/java/ru/qimix/Server.java
@@ -9,15 +9,57 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.logging.Handler;
+import java.util.logging.LogRecord;
 
 public class Server {
     protected final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
     protected final ExecutorService threadPool = Executors.newFixedThreadPool(64);
+    protected Map<Map<String,String>, Handler> handlerMap = new HashMap<Map<String,String>, Handler>();
+
+    public void fillHandlerMap(){
+        handlerMap.put((Map<String, String>) new HashMap<>().put("GET","/classic.html"), new Handler() {
+            public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
+                final var template = Files.readString(request.filePath);
+                final var content = template.replace(
+                        "{time}",
+                        LocalDateTime.now().toString()
+                ).getBytes();
+                responseStream.write((
+                        "HTTP/1.1 200 OK\r\n" +
+                                "Content-Type: " + request.mimeType + "\r\n" +
+                                "Content-Length: " + content.length + "\r\n" +
+                                "Connection: close\r\n" +
+                                "\r\n"
+                ).getBytes());
+                responseStream.write(content);
+                responseStream.flush();
+            }
+
+            @Override
+            public void publish(LogRecord record) {
+
+            }
+
+            @Override
+            public void flush() {
+
+            }
+
+            @Override
+            public void close() throws SecurityException {
+
+            }
+        });
+    }
 
     public void startServer() {
+        fillHandlerMap();
         try (final var serverSocket = new ServerSocket(9999)) {
             while (true) {
                 try {
@@ -67,22 +109,9 @@ public class Server {
         final var mimeType = Files.probeContentType(filePath);
 
         // special case for classic
-        if (path.equals("/classic.html")) {
-            final var template = Files.readString(filePath);
-            final var content = template.replace(
-                    "{time}",
-                    LocalDateTime.now().toString()
-            ).getBytes();
-            out.write((
-                    "HTTP/1.1 200 OK\r\n" +
-                            "Content-Type: " + mimeType + "\r\n" +
-                            "Content-Length: " + content.length + "\r\n" +
-                            "Connection: close\r\n" +
-                            "\r\n"
-            ).getBytes());
-            out.write(content);
-            out.flush();
-        }
+        Request request = new Request(parts[0], parts[1], filePath, mimeType, socket);
+        Handler handler = handlerMap.get((Map<String, String>) new HashMap<>().put("GET","/classic.html"))
+        System.out.println(handler);
 
         final var length = Files.size(filePath);
         out.write((
