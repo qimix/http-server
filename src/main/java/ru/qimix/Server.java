1b104b444f565ec7c3fd8683bd8039e31d2ed6d3
Vitaliy Murzin <murzinvit@yandex.ru>
2024-08-19 23:55:22 +0500

    add method fillMap

--- src/main/java/ru/qimix/Server.java
+++ src/main/java/ru/qimix/Server.java
@@ -14,8 +14,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.logging.Handler;
-import java.util.logging.LogRecord;
 
 public class Server {
     protected final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
@@ -24,41 +22,14 @@ public class Server {
 
     public void fillHandlerMap(){
         handlerMap.put((Map<String, String>) new HashMap<>().put("GET","/classic.html"), new Handler() {
-            public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
-                final var template = Files.readString(request.filePath);
-                final var content = template.replace(
-                        "{time}",
-                        LocalDateTime.now().toString()
-                ).getBytes();
-                responseStream.write((
-                        "HTTP/1.1 200 OK\r\n" +
-                                "Content-Type: " + request.mimeType + "\r\n" +
-                                "Content-Length: " + content.length + "\r\n" +
-                                "Connection: close\r\n" +
-                                "\r\n"
-                ).getBytes());
-                responseStream.write(content);
-                responseStream.flush();
-            }
-
-            @Override
-            public void publish(LogRecord record) {
-
-            }
-
-            @Override
-            public void flush() {
-
-            }
-
-            @Override
-            public void close() throws SecurityException {
-
+            public void handle(Request request, BufferedOutputStream responseStream) {
+                System.out.println("Hello world!");
             }
         });
     }
 
     public void startServer() {
+        System.out.println("Server started on port: 9999");
         fillHandlerMap();
         try (final var serverSocket = new ServerSocket(9999)) {
             while (true) {
@@ -110,8 +81,8 @@ public class Server {
 
         // special case for classic
         Request request = new Request(parts[0], parts[1], filePath, mimeType, socket);
-        Handler handler = handlerMap.get((Map<String, String>) new HashMap<>().put("GET","/classic.html"))
-        System.out.println(handler);
+        Handler handler = handlerMap.get((Map<String, String>) new HashMap<>().put("GET","/classic.html"));
+        handler.handle(request,new BufferedOutputStream(socket.getOutputStream()));
 
         final var length = Files.size(filePath);
         out.write((
