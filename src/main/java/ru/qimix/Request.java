809148e76dbcc5ed32cce4ab4bb4b9f4fbc99bba
Vitaliy Murzin <murzinvit@yandex.ru>
2024-08-21 04:43:55 +0500

    add addHandler

--- src/main/java/ru/qimix/Request.java
+++ src/main/java/ru/qimix/Request.java
@@ -1,35 +1,30 @@
 package ru.qimix;
 
-import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
 public class Request {
     String requestMethod;
+    String requestPath;
     String requestHeader;
     String requestBody;
-    Path filePath;
-    String mimeType;
 
     public Request(String requestMethod,
-                   String requestHeader,
-                   Path filePath,
-                   String mimeType
+                   String requestPath,
+                   String requestHeader
             ) {
         requestMethod = requestMethod;
+        requestPath = requestPath;
         requestHeader = requestHeader;
-        filePath = filePath;
-        mimeType = mimeType;
     }
     public Request(String requestMethod,
+                   String requestPath,
                    String requestHeader,
-                   String requestBody,
-                   Path filePath,
-                   Files mimeType
+                   String requestBody
          ) {
         requestMethod = requestMethod;
+        requestPath = requestPath;
         requestHeader = requestHeader;
         requestBody = requestBody;
-        mimeType = mimeType;
     }
 }
--- src/main/java/ru/qimix/Server.java
+++ src/main/java/ru/qimix/Server.java
@@ -9,6 +9,7 @@ import java.net.Socket;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,41 +19,87 @@ import java.util.concurrent.Executors;
 public class Server {
     protected final List<String> validPaths = List.of("/index.html", "/spring.svg", "/spring.png", "/resources.html", "/styles.css", "/app.js", "/links.html", "/forms.html", "/classic.html", "/events.html", "/events.js");
     protected final ExecutorService threadPool = Executors.newFixedThreadPool(64);
-    protected Map<Map<String,String>, Handler> handlerMap = new HashMap<Map<String,String>, Handler>();
+    protected Map<Map<String, String>, Handler> handlerMap = Collections.synchronizedMap(new HashMap<Map<String, String>, Handler>());
 
-    public void fillHandlerMap(){
-        handlerMap.put((Map<String, String>) new HashMap<>().put("GET","/classic.html"), new Handler() {
-            public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
-                    final var template = Files.readString(request.filePath);
-                    final var content = template.replace(
-                            "{time}",
-                            LocalDateTime.now().toString()
-                    ).getBytes();
-                    responseStream.write((
-                            "HTTP/1.1 200 OK\r\n" +
-                                    "Content-Type: " + request.mimeType + "\r\n" +
-                                    "Content-Length: " + content.length + "\r\n" +
-                                    "Connection: close\r\n" +
-                                    "\r\n"
-                    ).getBytes());
-                    responseStream.write(content);
-                    responseStream.flush();
-            }
-        });
+    Handler handler = new Handler() {
+        @Override
+        public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
+            System.out.println("Hello");
+            final var filePath = Path.of(".", "public", request.requestPath);
+            final var mimeType = Files.probeContentType(filePath);
+            final var template = Files.readString(filePath);
+            final var content = template.replace(
+                    "{time}",
+                    LocalDateTime.now().toString()
+            ).getBytes();
+            responseStream.write((
+                    "HTTP/1.1 200 OK\r\n" +
+                            "Content-Type: " + mimeType + "\r\n" +
+                            "Content-Length: " + content.length + "\r\n" +
+                            "Connection: close\r\n" +
+                            "\r\n"
+            ).getBytes());
+            responseStream.write(content);
+            responseStream.flush();
+        }
+    };
+
+    public void fillHandlerMap() {
+        handlerMap.put((Map<String, String>) new HashMap<>().put("GET", "/index.html"), handler);
+        handlerMap.put((Map<String, String>) new HashMap<>().put("GET", "/classic.html"), handler);
+        handlerMap.put((Map<String, String>) new HashMap<>().put("GET", "/spring.svg"), handler);
+        handlerMap.put((Map<String, String>) new HashMap<>().put("GET", "/spring.png"), handler);
+        handlerMap.put((Map<String, String>) new HashMap<>().put("GET", "/resources.html"), handler);
+        handlerMap.put((Map<String, String>) new HashMap<>().put("GET", "/styles.css"), handler);
+        handlerMap.put((Map<String, String>) new HashMap<>().put("GET", "/app.js"), handler);
+        handlerMap.put((Map<String, String>) new HashMap<>().put("GET", "/links.html"), handler);
+        handlerMap.put((Map<String, String>) new HashMap<>().put("GET", "/forms.html"), handler);
+        handlerMap.put((Map<String, String>) new HashMap<>().put("GET", "/events.html"), handler);
+        handlerMap.put((Map<String, String>) new HashMap<>().put("GET", "/events.js"), handler);
+    }
+
+
+    public void addHandler(String requestMethod, String requestPath, Handler handler) {
+
+
     }
 
     public void startServer() {
         System.out.println("Server started on port: 9999");
         fillHandlerMap();
-        try (final var serverSocket = new ServerSocket(9999)) {
+
+        try (var serverSocket = new ServerSocket(9999)) {
             while (true) {
                 try {
-                    final var socket = serverSocket.accept();
+                    var socket = serverSocket.accept();
                     threadPool.submit(new Runnable() {
                         @Override
                         public void run() {
                             try {
-                                connectionProcess(socket);
+                                var in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                                var out = new BufferedOutputStream(socket.getOutputStream());
+                                final var requestLine = in.readLine();
+                                final var parts = requestLine.split(" ");
+
+                                if (parts.length != 3) {
+                                    return;
+                                }
+
+                                final var path = parts[1];
+                                if (!validPaths.contains(path)) {
+                                    out.write((
+                                            "HTTP/1.1 404 Not Found\r\n" +
+                                                    "Content-Length: 0\r\n" +
+                                                    "Connection: close\r\n" +
+                                                    "\r\n"
+                                    ).getBytes());
+                                    out.flush();
+                                    return;
+                                } else {
+                                    Request request = new Request(parts[0], parts[1], parts[2]);
+                                    Handler handler = handlerMap.get((Map<String, String>) new HashMap<>().put(parts[0], parts[1]));
+                                    handler.handle(request, out);
+                                }
                             } catch (IOException e) {
                                 throw new RuntimeException(e);
                             }
@@ -93,9 +140,9 @@ public class Server {
         final var mimeType = Files.probeContentType(filePath);
 
         // special case for classic
-        Request request = new Request(parts[0], parts[1], filePath, mimeType);
-        Handler handler = handlerMap.get((Map<String, String>) new HashMap<>().put("GET","/classic.html"));
-        handler.handle(request,new BufferedOutputStream(socket.getOutputStream()));
+        Request request = new Request(parts[0], parts[1], parts[3]);
+        Handler handler = handlerMap.get((Map<String, String>) new HashMap<>().put("GET", "/classic.html"));
+        handler.handle(request, new BufferedOutputStream(socket.getOutputStream()));
 
         final var length = Files.size(filePath);
         out.write((
