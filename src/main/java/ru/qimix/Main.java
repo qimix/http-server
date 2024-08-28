098057fcc2c3653e318edd741cce8c1ef6e481e9
Vitaliy Murzin <murzinvit@yandex.ru>
2024-08-29 00:42:06 +0500

    add message.html get

--- src/main/java/ru/qimix/Main.java
+++ src/main/java/ru/qimix/Main.java
@@ -1,6 +1,10 @@
 package ru.qimix;
 
 import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.time.LocalDateTime;
 
 public class Main {
     public static void main(String[] args) {
@@ -8,8 +12,25 @@ public class Main {
 
             // добавление хендлеров (обработчиков)
             server.addHandler("GET", "/messages", new Handler() {
-                public void handle(Request request, BufferedOutputStream responseStream) {
-                    // TODO: handlers code
+                public void handle(Request request, BufferedOutputStream responseStream) throws IOException {
+                    System.out.println("Запрос обработан: " + request.requestMethod + " - " + request.requestPath);
+                    final var filePath = Path.of(".", "public", request.requestPath);
+                    final var mimeType = Files.probeContentType(filePath);
+                    final var template = Files.readString(filePath);
+                    final var content = template.replace(
+                            "{time}",
+                            LocalDateTime.now().toString()
+                    ).getBytes();
+                    responseStream.write((
+                            "HTTP/1.1 200 OK\r\n" +
+                                    "Content-Type: " + mimeType + "\r\n" +
+                                    "Content-Length: " + content.length + "\r\n" +
+                                    "Connection: close\r\n" +
+                                    "\r\n"
+                    ).getBytes());
+                    responseStream.write(content);
+                    responseStream.flush();
+                    responseStream.close();
                 }
             });
             server.addHandler("POST", "/messages", new Handler() {
